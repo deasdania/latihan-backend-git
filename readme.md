a4b2bbfd50c52e5be2459074279200505d154be6
deadaniax <dea@x-foundation.io>
2024-11-13 21:32:57 +0700

    Fix bugs for release 1.0.0

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+
+1. Install dependencies: `npm install`
+2. Build the project: `npm run build`
+3. Start the application: `npm start`
+
--- src/index.js
+++ src/index.js
@@ -1,50 +1,51 @@
-import express, { Request, Response } from 'express';
-
-const app = express();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const port = 3000;
-
-app.use(express.json());
-
+app.use(express_1.default.json());
 // Endpoint untuk register user
-app.post('/register', (req: Request, res: Response) => {
+app.post('/register', (req, res) => {
     const { username, password } = req.body;
     // Logika untuk register user
     res.send('User registered');
 });
-
 // Endpoint untuk login user
-app.post('/login', (req: Request, res: Response) => {
+app.post('/login', (req, res) => {
     const { username, password } = req.body;
     // Logika untuk login user
     res.send('User logged in');
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
 // Endpoint untuk menambah produk
-app.post('/products', (req: Request, res: Response) => {
+app.post('/products', (req, res) => {
     const { name, price } = req.body;
     // Logika untuk menambah produk
     res.send('Product added');
 });
-
 // Endpoint untuk mendapatkan semua produk
-app.get('/products', (req: Request, res: Response) => {
+app.get('/products', (req, res) => {
     // Logika untuk mendapatkan semua produk
     res.send('List of products');
 });
 
 // Endpoint untuk membuat order
-app.post('/orders', (req: Request, res: Response) => {
+app.post('/orders', (req, res) => {
     const { productId, quantity } = req.body;
     // Logika untuk membuat order
     res.send('Order created');
 });
-
 // Endpoint untuk mendapatkan semua order
-app.get('/orders', (req: Request, res: Response) => {
+app.get('/orders', (req, res) => {
     // Logika untuk mendapatkan semua order
     res.send('List of orders');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
